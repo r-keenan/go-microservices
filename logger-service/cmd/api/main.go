b0f18499b26bdaeff3d2d03a27da2918ea501d13
r-keenan <ross.keenan@protonmail.com>
2024-09-19 11:57:50 -0500

    Added connection to mongo, model, and ability to insert a log into mongo.

---
+++ logger-service/cmd/api/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+const (
+	webPort  = "80"
+	rpcPort  = "5001"
+	grpcPort = "50001"
+	mongoURL = "mongodb://mongo:27017"
+)
+
+var client *mongo.Client
+
+type Config struct {
+}
+
+func main() {
+	// connect to mongo
+	mongoClient, err := connectToMongo()
+	if err != nil {
+		log.Panic(err)
+	}
+	client = mongoClient
+
+	// create a context in order to disconnect
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+
+	// close connection
+	defer func() {
+		if err = client.Disconnect(ctx); err != nil {
+			panic(err)
+		}
+	}()
+
+}
+
+func connectToMongo() (*mongo.Client, error) {
+
+	// connection options
+	clientOptions := options.Client().ApplyURI(mongoURL)
+	// obviously do not hardcode these in prod
+	clientOptions.SetAuth(options.Credential{
+		Username: "admin",
+		Password: "password",
+	})
+	// connect
+	c, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Panicln("error connecting", err)
+		return nil, err
+	}
+
+	return c, nil
+}
